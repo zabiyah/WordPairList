b1206a80eb0110d7b319b7ca4e42e93a6285f442
Quincy Holmer <65032461+mrHolmer@users.noreply.github.com>
2025-04-21 16:10:14 -0400

    Add files via upload

---
+++ WordPair.java
@@ -0,0 +1,13 @@
+public class WordPair {
+    /** Constructs a WordPair object. */
+    public WordPair(String first, String second) {
+        /* implementation not shown */ }
+
+    /** Returns the first string of this WordPair object. */
+    public String getFirst() {
+        /* implementation not shown */ }
+
+    /** Returns the second string of this WordPair object. */
+    public String getSecond() {
+        /* implementation not shown */ }
+}
---
+++ WordPairList.java
@@ -0,0 +1,18 @@
+
+public class WordPairList {
+    /** The list of word pairs, initialized by the constructor. */
+    private ArrayList<WordPair> allPairs;
+
+    /**
+     * Constructs a WordPairList object as described in part (a).
+     * Precondition: words.length >= 2
+     */
+    public WordPairList(String[] words) {
+        /* to be implemented in part (a) */ }
+
+    /**
+     * Returns the number of matches as described in part (b).
+     */
+    public int numMatches() {
+        /* to be implemented in part (b) */ }
+}
